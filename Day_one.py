84f7a85518c29cd19cab636710d5adbd50f717c1
AbduRehman101 <mrabdurehman101@gmail.com>
2024-12-23 19:40:20 +0500

    Day-One

---
+++ Day_one.py
@@ -0,0 +1,23 @@
+# Wirte a program to input 2 number & print it's sum
+num1 = int(input("Enter The Number One: "))
+num2 = int(input("Enter The Number Two: "))
+res = num1 + num2
+print("Here is a result:",res)
+
+
+#WAP to input side of a square & print its area
+Side = int(input("Enter The Side Of Square:"))
+Area = Side * Side
+print("Here is the Area of Square:",Area)
+
+#WAP to Input 2 Floating point numbers &`print their average.
+num_one = float(input("Enter The Num_one:"))
+num_Two =  float(input("Enter The Num_two:"))
+average = (num_one + num_Two) / 2
+print("Here Is the Result of average:",average)
+
+
+#WAP to input 2 int number a and b. Print True if a is greater than or equal to b. if not print False.
+a = int(input("Enter The A val:"))
+b = int(input("Enter The B val:"))
+print(a>=b)
